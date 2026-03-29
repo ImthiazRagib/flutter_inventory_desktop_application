f0604b4241f37a9f0e88e1608fbbff6d3217a357
imthiazRagib <imthiazragib@gmail.com>
2026-03-29 20:40:43 +0400

    Remove unused import of go_router in ReportsPage and SettingsPage

--- lib/features/screens/reports/reports.dart
+++ lib/features/screens/reports/reports.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inventory_desktop_application/app/layout/app_layout.dart';
-import 'package:go_router/go_router.dart';
 
 class ReportsPage extends StatefulWidget {
   const ReportsPage({super.key});
--- lib/features/screens/settings/settings.dart
+++ lib/features/screens/settings/settings.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inventory_desktop_application/app/layout/app_layout.dart';
-import 'package:go_router/go_router.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
